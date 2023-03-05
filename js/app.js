9b403974731f3dd636c6604e7015dd7f434c3130
dg1223 <alavi1223@hotmail.com>
2023-03-04 19:18:38 -0500

    temperature updated from API

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@
 const API_KEY = `7fc33929181b0e5fd0aa66d58fcc5c3a`;
 
 const loadTemperature = (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayTemperature(data));
 };
 
-loadTemperature("Dhaka");
+const displayTemperature = (data) => {
+  const temperature = document.getElementById("temperature");
+  console.log(data.main.temp);
+  temperature.innerText = `${data.main.temp}`;
+};
+
+loadTemperature("dhaka");
